e1c6636fc7e631aa3f2723ff89854dcb272ea5b5
Ralph Gasser <rg@pontius.ch>
2019-06-27 06:34:22 +0200

    - Corrected mapping of FloatValue and FloatArrayValue to respective SQL type. - Added support for nullability.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/calcite/adapter/CottontailExtensions.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/calcite/adapter/CottontailExtensions.kt
@@ -15,19 +15,19 @@ import org.apache.calcite.sql.type.SqlTypeName
  * @return [RelDataType] that corresponds to given [ColumnDef]
  */
 fun ColumnDef<*>.toSQLType(typeFactory: RelDataTypeFactory) = when (this.type) {
-    is StringType -> Entry(this.name.last(), typeFactory.createSqlType(SqlTypeName.VARCHAR))
-    is BooleanType -> Entry(this.name.last(), typeFactory.createSqlType(SqlTypeName.BOOLEAN))
-    is ByteType -> Entry(this.name.last(), typeFactory.createSqlType(SqlTypeName.TINYINT))
-    is ShortType -> Entry(this.name.last(), typeFactory.createSqlType(SqlTypeName.SMALLINT))
-    is IntType -> Entry(this.name.last(),typeFactory.createSqlType(SqlTypeName.INTEGER))
-    is LongType -> Entry(this.name.last(),typeFactory.createSqlType(SqlTypeName.BIGINT))
-    is FloatType -> Entry(this.name.last(),typeFactory.createSqlType(SqlTypeName.FLOAT))
-    is DoubleType -> Entry(this.name.last(),typeFactory.createSqlType(SqlTypeName.DOUBLE))
-    is IntArrayType -> Entry(this.name.last(),typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.INTEGER), this.size.toLong()))
-    is LongArrayType -> Entry(this.name.last(),typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.BIGINT), this.size.toLong()))
-    is FloatArrayType -> Entry(this.name.last(),typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.FLOAT), this.size.toLong()))
-    is DoubleArrayType -> Entry(this.name.last(),typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.DOUBLE), this.size.toLong()))
-    is BooleanArrayType -> Entry(this.name.last(),typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.BOOLEAN), this.size.toLong()))
+    is StringType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.VARCHAR), this.nullable))
+    is BooleanType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BOOLEAN), this.nullable))
+    is ByteType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.TINYINT), this.nullable))
+    is ShortType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.SMALLINT), this.nullable))
+    is IntType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.INTEGER), this.nullable))
+    is LongType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BIGINT), this.nullable))
+    is FloatType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.REAL), this.nullable))
+    is DoubleType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.DOUBLE), this.nullable))
+    is IntArrayType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createArrayType(typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.INTEGER), false), this.size.toLong()), this.nullable))
+    is LongArrayType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createArrayType(typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BIGINT), false), this.size.toLong()), this.nullable))
+    is FloatArrayType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createArrayType(typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.REAL), false), this.size.toLong()), this.nullable))
+    is DoubleArrayType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createArrayType(typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.DOUBLE), false), this.size.toLong()), this.nullable))
+    is BooleanArrayType -> Entry(this.name.last(),typeFactory.createTypeWithNullability(typeFactory.createArrayType(typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BOOLEAN), false), this.size.toLong()), this.nullable))
 }
 
 /**
@@ -43,11 +43,11 @@ fun Type<*>.toSQLType(typeFactory: RelDataTypeFactory) : RelDataType = when (thi
     is ShortType -> typeFactory.createSqlType(SqlTypeName.SMALLINT)
     is IntType -> typeFactory.createSqlType(SqlTypeName.INTEGER)
     is LongType -> typeFactory.createSqlType(SqlTypeName.BIGINT)
-    is FloatType -> typeFactory.createSqlType(SqlTypeName.FLOAT)
+    is FloatType -> typeFactory.createSqlType(SqlTypeName.REAL)
     is DoubleType -> typeFactory.createSqlType(SqlTypeName.DOUBLE)
     is IntArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.INTEGER), Long.MAX_VALUE)
     is LongArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.BIGINT), Long.MAX_VALUE)
-    is FloatArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.FLOAT), Long.MAX_VALUE)
+    is FloatArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.REAL), Long.MAX_VALUE)
     is DoubleArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.DOUBLE), Long.MAX_VALUE)
     is BooleanArrayType -> typeFactory.createArrayType(typeFactory.createSqlType(SqlTypeName.BOOLEAN), Long.MAX_VALUE)
 }

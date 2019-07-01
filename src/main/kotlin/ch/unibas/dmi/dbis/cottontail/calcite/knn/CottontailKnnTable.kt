8c8a24580271430faf1554ce064ce8c27b1c308f
Ralph Gasser <rg@pontius.ch>
2019-07-01 18:12:36 +0200

    Updated the structure of the CottontailKnnTable class.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/calcite/knn/CottontailKnnTable.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/calcite/knn/CottontailKnnTable.kt
@@ -3,9 +3,8 @@ package ch.unibas.dmi.dbis.cottontail.calcite.knn
 import ch.unibas.dmi.dbis.cottontail.calcite.adapter.toSQLType
 import ch.unibas.dmi.dbis.cottontail.math.knn.ComparablePair
 import ch.unibas.dmi.dbis.cottontail.math.knn.HeapSelect
-import ch.unibas.dmi.dbis.cottontail.model.values.DoubleValue
-import ch.unibas.dmi.dbis.cottontail.model.values.IntValue
 import ch.unibas.dmi.dbis.cottontail.model.values.Value
+
 import org.apache.calcite.DataContext
 import org.apache.calcite.linq4j.*
 import org.apache.calcite.rel.RelCollation
@@ -21,18 +20,27 @@ import org.apache.calcite.schema.impl.AbstractTable
 import org.apache.calcite.sql.type.SqlTypeName
 import org.apache.calcite.util.ImmutableBitSet
 
+/**
+ * A Calcite [Table] implementation that wraps the results of a kNN lookup.
+ *
+ * @author Ralph Gasser
+ * @version 1.0
+ */
 class CottontailKnnTable (backingList: List<HeapSelect<ComparablePair<Value<*>, Double>>>) : AbstractTable(), ScannableTable {
 
+    /** The [Type] of the (first) ID field. */
+    private val idType = backingList.first().peek()!!.first.type
+
     /** Flattens the backing list into a single list containing all results. However, keeps the relationship between query vector and entry. */
-    val list = backingList.mapIndexed { idx, heap -> heap.heap().map { arrayOf(it.first, DoubleValue(it.second), IntValue(idx)) } }.flatten()
+    private  val list = backingList.mapIndexed { idx, heap -> (0 until heap.size).map { i -> arrayOf(heap[i].first.value, heap[i].second, idx) } }.flatten()
 
     /**
+     * Implements the scanning operation as required by [ScannableTable].
      *
+     * @param root The [DataContext] in which this scanning operation takes place
      */
-    override fun scan(root: DataContext?): Enumerable<Array<Any>> = object: AbstractEnumerable<Array<Any>>() {
-        override fun enumerator(): Enumerator<Array<Any>> = object: TransformedEnumerator<Array<Value<*>>, Array<Any>>(Linq4j.iterableEnumerator(this@CottontailKnnTable.list)) {
-            override fun transform(from: Array<Value<*>>?): Array<Any> = from as Array<Any>
-        }
+    override fun scan(root: DataContext): Enumerable<Array<Any>> = object: AbstractEnumerable<Array<Any>>() {
+        override fun enumerator(): Enumerator<Array<Any>> = Linq4j.iterableEnumerator(this@CottontailKnnTable.list)
     }
 
     /**
@@ -41,8 +49,8 @@ class CottontailKnnTable (backingList: List<HeapSelect<ComparablePair<Value<*>, 
      * @return [RelDataType] of this [CottontailKnnTable].
      */
     override fun getRowType(typeFactory: RelDataTypeFactory): RelDataType = typeFactory.createStructType(
-        listOf(this.list.first()[0].type.toSQLType(typeFactory), typeFactory.createSqlType(SqlTypeName.DOUBLE), typeFactory.createSqlType(SqlTypeName.INTEGER)),
-        listOf("id", "distance", "idx")
+        listOf(this.idType.toSQLType(typeFactory), typeFactory.createSqlType(SqlTypeName.FLOAT), typeFactory.createSqlType(SqlTypeName.INTEGER)),
+        listOf("id", "distance", "query")
     )
 
     /**
